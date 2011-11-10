990487754bccd8ce0c5b9ab01b425531e7d03538
huangyingw@linux_programming <huangyingw@gmail.com>
2011-11-10 21:58:42 +0800

    n

--- dividePrime.cc
+++ dividePrime.cc
@@ -1,10 +1,13 @@
+// dividePrime.cpp : Defines the entry point for the console application.
+//
+
 #include"stdio.h"
 #include"stdlib.h"
 #include<iostream>
 
 using namespace std;
 
-void dividePrime(int num)
+void dividePrime(int num, int result[])
 {
   int fac=2;
   if ( 1 == num)
@@ -14,14 +17,36 @@ void dividePrime(int num)
   }
   while ( 0!=num%fac )
     fac++;
-  cout<<fac<<"*";
-  dividePrime(num/fac);
+  result[0]=fac;
+  dividePrime(num/fac,++result);
 }
 
-int main() 
+int main()
 {
-  dividePrime(12321);
-  dividePrime(12325);
-  dividePrime(12320);
+  int result[1000]={0};
+  dividePrime(12321,result);
+  cout<<"12321"<<"=";
+  for (int i=0;result[i]!=0;i++)
+  {
+    cout<<result[i]<<"*";
+  }
+  cout<<endl;
+
+  dividePrime(12325,result);
+  cout<<"12325"<<"=";
+  for (int i=0;result[i]!=0;i++)
+  {
+    cout<<result[i]<<"*";
+  }
+  cout<<endl;
+
+  dividePrime(12320,result);
+  cout<<"12320"<<"=";
+  for (int i=0;result[i]!=0;i++)
+  {
+    cout<<result[i]<<"*";
+  }
+  cout<<endl;
   return 0;
 }
+
